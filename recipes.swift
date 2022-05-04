c78a69cbda28adde1eea11f6a00ef4a3e5e5179d
Sai Deep Konduri <kondurisaideep@gmail.com>
2022-05-03 19:08:37 -0500

    Create recipes.swift

---
+++ recipes.swift
@@ -0,0 +1,4 @@
+
+let breakfast: [Recipe] = [ Recipe(name: "Dosa", description: "blah blah", category: "", ingrediants: ["Urad dal"], imagePath: "dosa"), ]
+let lunch: [Recipe] = [ Recipe(name: "Biryani", description: "blah blah", category: "", ingrediants: ["Urad dal"], imagePath: "biryani")]
+let dinner: [Recipe] = [ Recipe(name: "Curd Rice", description: "blah blah", category: "", ingrediants: ["Urad dal"], imagePath: "dosa")]
